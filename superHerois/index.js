40f531c52145d9937875ede77430ecb7c3d9ea8a
Matheus Prado <matheusfilipe573@gmail.com>
2022-03-31 23:29:07 -0300

    Primeiro commit

---
+++ superHerois/index.js
@@ -0,0 +1,12 @@
+const herois =[
+    {
+        nome: "Super Thiagão",
+        poder: "Cabeça grande"
+    },
+    {
+        nome: "Super Pri",
+        poder: "Chegar cedo"
+    }
+]
+
+module.exports = herois;
